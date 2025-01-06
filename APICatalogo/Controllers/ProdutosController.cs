8b1f4cd70e67ffebd3f3110d01917df37010b0b1
Marcelo Rodrigues <ruysmarcelo@gmail.com>
2025-01-06 15:58:50 -0300

    controller e action GetAll criados

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,28 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult<IEnumerable<Produto>> Get() 
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados!");
+            }
+            return produtos;
+        }
+    }
+}
