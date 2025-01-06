5c56f4f6d785a6c01d5bb3b5f573a4475fc22865
Marcelo Rodrigues <ruysmarcelo@gmail.com>
2025-01-06 15:18:09 -0300

    data annotations adicionados e migracoes realizadas

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
         public Categoria()
@@ -10,7 +13,15 @@ namespace APICatalogo.Models
             Produtos = new Collection<Produto>();
         }
 
+        [Key]
         public int CategoriaId { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string? Nome { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImagemUrl { get; set; }
 
         public ICollection<Produto> Produtos { get; set; }
