d33a6bfa25b12d84fd940099385ed9ddfdda7b75
Marcelo Rodrigues <ruysmarcelo@gmail.com>
2025-01-06 21:23:33 -0300

    validacao personalizada com IValidatableObject

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -6,14 +6,14 @@ using APICatalogo.Validations;
 namespace APICatalogo.Models
 {
     [Table("Produtos")]
-    public class Produto
+    public class Produto : IValidatableObject
     {
         [Key]
         public int ProdutoId { get; set; }
 
         [Required]
         [StringLength(80)]
-        [PrimeiraLetraMaiuscula]
+        // [PrimeiraLetraMaiuscula]
         public string? Nome { get; set; }
         
         [Required]
@@ -34,5 +34,31 @@ namespace APICatalogo.Models
         
         [JsonIgnore]
         public Categoria? Categoria { get; set; }
+        
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(this.Nome))
+            {
+                var primeiraLetra = this.Nome[0].ToString();
+                if (primeiraLetra != primeiraLetra.ToUpper())
+                {
+                    yield return new
+                        ValidationResult("A primeira letra do produto deve ser maiúscula",
+                        new[]
+                        { nameof(this.Nome) }
+                        );
+                }
+            }
+
+            if (this.Estoque <= 0)
+            {
+                yield return new
+                       ValidationResult("O estoque deve ser maior que zero",
+                       new[]
+                       { nameof(this.Estoque) }
+                       );
+            }
+        }
+
     }
 }
