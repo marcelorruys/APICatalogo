35d02eae06f75c98327b390ad9caebdd4ba933c6
Marcelo Rodrigues <ruysmarcelo@gmail.com>
2025-01-07 12:35:30 -0300

    filtro personalizado para tratamento de excecoes e middleware para logging

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -11,80 +11,43 @@ namespace APICatalogo.Controllers;
 public class CategoriasController : ControllerBase
 {
     private readonly AppDbContext _context;
-    private readonly IConfiguration _configuration;
+    private readonly ILogger<CategoriasController> _logger;
 
-    public CategoriasController(AppDbContext context,
-                                IConfiguration configuration)
+    public CategoriasController(AppDbContext context, ILogger<CategoriasController> logger)
     {
         _context = context;
-        _configuration = configuration;
-    }
-
-    [HttpGet("LerArquivoConfiguracao")]
-    public string GetValores()
-    {
-        var valor1 = _configuration["chave1"];
-        var valor2 = _configuration["chave2"];
-
-        var secao1 = _configuration["secao1:chave2"];
-
-        return $"Chave1 = {valor1}  \nChave2 = {valor2}  \nSeção1 => Chave2 = {secao1}";
+        _logger = logger;
     }
 
     [HttpGet]
     [ServiceFilter(typeof(ApiLoggingFilter))]
     public ActionResult<IEnumerable<Categoria>> GetAll()
     {
-        try
-        {
             return _context.Categorias.AsNoTracking().ToList();
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                "Ocorreu um problema ao tratar a sua solicitação.");
-        }
     }
     
     [HttpGet("produtos")]
     public ActionResult<IEnumerable<Categoria>> GetAllWithProducts()
     {
-        try
-        {
             return _context.Categorias.Include(p => p.Produtos).ToList();
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                "Ocorreu um problema ao tratar a sua solicitação.");
-        }
     }
 
     [HttpGet("{id:int:min(1)}", Name = "GetByIdCategoria")]
     public ActionResult<Categoria> GetById(int id)
     {
-        try
+        var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+        
+        if (categoria == null)
         {
-            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
-
-            if (categoria == null)
-            {
-                return NotFound($"Categoria com id= {id} não encontrada...");
-            }
-            return Ok(categoria);
+            return NotFound($"Categoria com id= {id} não encontrada...");
         }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                       "Ocorreu um problema ao tratar a sua solicitação.");
-        }
+        
+        return Ok(categoria);
     }
 
     [HttpPost]
     public ActionResult Create(Categoria categoria)
     {
-        try
-        {
             if (categoria is null)
                 return BadRequest("Dados inválidos");
 
@@ -93,54 +56,35 @@ public class CategoriasController : ControllerBase
 
             return new CreatedAtRouteResult("GetByIdCategoria",
                 new { id = categoria.CategoriaId }, categoria);
-
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                      "Ocorreu um problema ao tratar a sua solicitação.");
-        }
     }
 
     [HttpPut("{id:int:min(1)}")]
     public ActionResult Update(int id, Categoria categoria)
     {
-        try
+        if (id != categoria.CategoriaId)
         {
-            if (id != categoria.CategoriaId)
-            {
-                return BadRequest("Dados inválidos");
-            }
-            _context.Entry(categoria).State = EntityState.Modified;
-            _context.SaveChanges();
-            return Ok(categoria);
+            return BadRequest("Dados inválidos");
         }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                   "Ocorreu um problema ao tratar a sua solicitação.");
-        }
+
+        _context.Entry(categoria).State = EntityState.Modified;
+        _context.SaveChanges();
+            
+        return Ok(categoria);
     }
 
     [HttpDelete("{id:int:min(1)}")]
     public ActionResult Delete(int id)
     {
-        try
-        {
-            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+        var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
 
-            if (categoria == null)
-            {
-                return NotFound($"Categoria com id={id} não encontrada...");
-            }
-            _context.Categorias.Remove(categoria);
-            _context.SaveChanges();
-            return Ok(categoria);
-        }
-        catch (Exception)
+        if (categoria == null)
         {
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                           "Ocorreu um problema ao tratar a sua solicitação.");
+            return NotFound($"Categoria com id={id} não encontrada...");
         }
+
+        _context.Categorias.Remove(categoria);
+        _context.SaveChanges();
+
+        return Ok(categoria);
     }
 }
--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -2,13 +2,22 @@ using System.Text.Json.Serialization;
 using APICatalogo.Context;
 using APICatalogo.Extensions;
 using APICatalogo.Filters;
+using APICatalogo.Logging;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers().AddJsonOptions(options =>
-    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(ApiExceptionFilter));
+})
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,6 +30,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddScoped<ApiLoggingFilter>();
 
+builder.Logging.AddProvider(new CustomLoggerProvider(new CustomLoggerProviderConfiguration
+{
+    LogLevel = LogLevel.Information
+}));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
